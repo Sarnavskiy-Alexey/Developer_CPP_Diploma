47a3d5c49e374db459274a0e69b9545ecfecbf80
Sarnavskiy Alexey <alexey.sarnavskiy@ya.ru>
2024-07-02 14:04:11 +0300

    Скорректирован алгоритм индексации: теперь с потоками.

--- source/src/InvertedIndex.cpp
+++ source/src/InvertedIndex.cpp
@@ -5,9 +5,12 @@
 #include "InvertedIndex.hpp"
 #include <algorithm>
 #include <sstream>
+#include <thread>
+#include <mutex>
 
 void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs)
 {
+    std::mutex freq_dictionary_mutex;
     /* очистка имеющихся данных */
     this->docs.clear();
     freq_dictionary.clear();
@@ -18,9 +21,8 @@ void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs)
     /* заполнение новыми данными файла docs */
     std::copy(input_docs.begin(), input_docs.end(), this->docs.begin());
 
-    /* заполнение словаря */
-    for (size_t i = 0; i < input_docs.size(); i++)
-    {
+    /* лямбда-функция для добавления в поток */
+    auto func = [&](size_t i){
         std::stringstream buffer_stream(input_docs[i]);
         std::string word;
         int word_count = 1;
@@ -36,6 +38,8 @@ void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs)
                 word.resize(100);
             }
             word_count++;
+
+            freq_dictionary_mutex.lock();
 
             /* если ключевое слово имеется в словаре, то... */
             if (this->freq_dictionary.find(word) != this->freq_dictionary.end())
@@ -63,7 +67,24 @@ void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs)
                 this->freq_dictionary[word] = {};
                 this->freq_dictionary[word].push_back({ .doc_id = i, .count = 1 });
             }
+
+            freq_dictionary_mutex.unlock();
         }
+    };
+
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < 8; i++)
+    {
+    }
+
+    /* заполнение словаря */
+    for (size_t i = 0; i < input_docs.size(); i++)
+    {
+        threads.push_back(std::thread(func, i));
+    }
+    for (size_t i = 0; i < input_docs.size(); i++)
+    {
+        threads[i].join();
     }
 }
 
